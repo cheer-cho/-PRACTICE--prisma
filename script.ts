35d1418d4c15459fcfd38efc4bef4bb1e70d7f1f
Cheer Chokwittaya <cheer.chokwit@gmail.com>
2024-08-27 18:00:26 -0500

    [2] - first basic prima code to test create and db connection

---
+++ script.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({ data: { name: 'Cheer' } });
+  const users = await prisma.user.findMany();
+  console.log(users);
+}
+
+main()
+  .catch((e) => {
+    console.log(e.message);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
