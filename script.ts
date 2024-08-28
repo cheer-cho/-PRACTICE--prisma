4656c3fdfe14409bcf9356648d2c8963dc3f9eff
Cheer Chokwittaya <cheer.chokwit@gmail.com>
2024-08-27 19:53:57 -0500

    run migration

--- script.ts
+++ script.ts
@@ -3,14 +3,16 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  const user = await prisma.user.create({ data: { name: 'Cheer' } });
-  const users = await prisma.user.findMany();
-  console.log(users);
+  // const user = await prisma.user.create({ data: { name: 'Cheer' } });
+  // const users = await prisma.user.findMany();
+  // console.log(users);
+  await prisma.user.deleteMany();
 }
 
 main()
   .catch((e) => {
     console.log(e.message);
+    console.log('a');
   })
   .finally(async () => {
     await prisma.$disconnect();
